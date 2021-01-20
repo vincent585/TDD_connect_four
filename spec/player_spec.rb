f9394a77684e27ceff76236179a3b13f410a7479
Vincent Klimjack <vklimjack@gmail.com>
2021-01-20 07:22:23 +0000

    added player_spec description as tests not needed for Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+describe Player do
+  # tests not needed as the class will only contain instance variables set in initialize method
+end
+
+# Player class will contain:
+# - piece instance var
+# - player num instance var
+
+# Because I am not going to allow the player to choose their piece but rather assign one automatically,
+# there is no need to write tests for this class as there will be no methods to test.
