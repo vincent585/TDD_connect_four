a6b8f3f13103478414941b2b197068f79f85860c
Vincent Klimjack <vklimjack@gmail.com>
2021-01-22 05:58:46 +0000

    added tests for #full? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require './lib/board'
 
 describe Board do
   subject(:board) { described_class.new }
@@ -6,7 +7,7 @@ describe Board do
   describe '#game_over?' do
     context 'when there is a horizontal 4-in-a-row' do
       before do
-        board.instance_variable_set(:@cells, Array.new(6) {Array.new(7)})
+        board.instance_variable_set(:@cells, Array.new(6) { Array.new(7) })
         board.cells[5] = ['x', 'x', 'x', 'x', ' ', ' ', ' ']
       end
 
@@ -25,6 +26,36 @@ describe Board do
   end
 
   describe '#full?' do
-    # TODO
+    context 'when the board is new' do
+      before do
+        board.instance_variable_set(:@cells, Array.new(6) { Array.new(7, ' ') })
+      end
+
+      it 'is not full' do
+        expect(board).not_to be_full
+      end
+    end
+
+    context 'when the board is partially played' do
+      before do
+        board.instance_variable_set(:@cells, Array.new(6) { Array.new(7) })
+        board.cells[5] = %w[x o x x o x o]
+        board.cells[4] = %w[x o x x o x o]
+      end
+
+      it 'is not full' do
+        expect(board).not_to be_full
+      end
+    end
+
+    context 'when the board is full' do
+      before do
+        board.instance_variable_set(:@cells, Array.new(6) { Array.new(7, 'x') })
+      end
+
+      it 'is full' do
+        expect(board).to be_full
+      end
+    end
   end
 end
