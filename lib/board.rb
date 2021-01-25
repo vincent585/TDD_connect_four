e57366d629906e01eb1472fed1cd4bfec5b9cd99
Vincent Klimjack <vklimjack@gmail.com>
2021-01-25 16:52:14 +0000

    added #valid_move? and #column_not_full? methods, tests passing for #valid_move?

--- lib/board.rb
+++ lib/board.rb
@@ -14,7 +14,15 @@ class Board
   def full?
     cells.all? { |row| return true unless row.include?(' ') }
   end
-end
 
-x = Board.new
-x.show_board
+  def valid_move?(move)
+    return true if move.match?(/^[1-7]{1}$/) && column_not_full?(move)
+
+    false
+  end
+
+  def column_not_full?(move)
+    transposed = cells.transpose
+    transposed[move.to_i - 1].include?(' ')
+  end
+end
